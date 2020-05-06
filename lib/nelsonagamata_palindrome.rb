6e9b9fda607f02fdbd154f493a3324df71788cd7
Nelson Agamata <nelson.s.agamata@gmail.com>
2020-05-06 21:24:59 +0800

    Factor into module, add integer palindrome detector

--- lib/nelsonagamata_palindrome.rb
+++ lib/nelsonagamata_palindrome.rb
@@ -1,16 +1,23 @@
 require "nelsonagamata_palindrome/version"
 
-class String
-
+module NelsonagamataPalindrome
   #Returns true for palindrome, false otherwise
   def palindrome?
     processed_content == processed_content.reverse
   end
- 
+
   private
 
   #Returns content for palindrome testing
   def processed_content
-    self.scan(/[a-z]/i).join.downcase
+    self.to_s.scan(/[a-z\d]/i).join.downcase
   end
+end
+
+class String
+  include NelsonagamataPalindrome
+end
+
+class Integer
+  include NelsonagamataPalindrome
 end
